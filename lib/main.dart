165db2e474f9722d365554b4b72f49810a0368c9
Marshall Shen <shen.marshall@gmail.com>
2020-11-27 17:37:37 -0600

    add expansion tile

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amercia Civics',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.lightBlue
       ),
       home: MyHomePage(title: 'U.S. Citizenship Civic tests'),
     );
@@ -47,22 +47,44 @@ class _MyHomePageState extends State<MyHomePage> {
             future: fetchCardsData(context),
             builder: (BuildContext context, snapshot) {
               if (snapshot.hasData) {
-                return ListView.builder(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: snapshot.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Container(
-                      height: 50,
-                      child: Center(child: Text(snapshot.data[index].question))
-                    );
-                  },
-                );
+                return _buildCardsListView(snapshot.data);
               } else {
                 return Center(child: CircularProgressIndicator());
               }
             }
         ),
       )
+    );
+  }
+
+  Widget _buildCardsListView(List<StudyCard> cards) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemCount: cards.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Card(
+            child: Padding(
+                padding: EdgeInsets.only(top: 16.0, left: 6.0, right: 6.0, bottom: 6.0),
+                child: ExpansionTile(
+                    title: Text(cards[index].question),
+                    children: [
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.30,
+                          child: ListView.builder(
+                            itemCount: cards[index].answers.length,
+                            itemBuilder: (BuildContext build, int answerIndex) {
+                              return ListTile(
+                                leading: Icon(Icons.check),
+                                title: Text(cards[index].answers[answerIndex]),
+                              );
+                            },
+                          )
+                      )
+                    ]
+                )
+            )
+        );
+      },
     );
   }
 }
